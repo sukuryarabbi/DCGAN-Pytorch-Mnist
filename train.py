c6b9a77a5bca8aac7408fecbf072662d77aa2a58
sukuryarabbi <sukruu.aydin001@gmail.com>
2025-05-09 06:42:26 +0300

    fixed

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from utils import utils_weights
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 LEARNING_RATE = 2e-4
-BATCH_SIZE = 126
+BATCH_SIZE = 128
 IMAGE_SIZE = 64
 CHANNELS_IMG = 1
 Z_DIM = 100
@@ -21,11 +21,11 @@ NUM_EPOCHS = 5
 FEATURES_DISC = 64
 FEATURES_GEN = 64
 
-transforms = transforms.Compose(
-    [transforms.Resize(IMAGE_SIZE),
-     transforms.ToTensor(),
-     transforms.Normalize(
-         ([0.5 for _ in range(CHANNELS_IMG)],[0.5 for _ in range(CHANNELS_IMG)])
+transforms = transforms.Compose([
+    transforms.Resize(IMAGE_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(
+         [0.5 for _ in range(CHANNELS_IMG)],[0.5 for _ in range(CHANNELS_IMG)]
          )
      ])
 
@@ -42,7 +42,7 @@ opt_disc = optim.Adam(disc.parameters(),lr=LEARNING_RATE, betas = (0.5,0.999))
 
 criterion = nn.BCELoss()
 
-fixed_noise = torch.rand(32,Z_DIM,1,1).to(device)
+fixed_noise = torch.randn(32,Z_DIM,1,1).to(device)
 writer_real = SummaryWriter(f"logs/real")
 writer_fake = SummaryWriter(f"logs/fake")
 step = 0
@@ -53,22 +53,23 @@ disc.train()
 for epoch in range(NUM_EPOCHS):
     for batch_idx,(real,_) in enumerate(dataloader):
         real = real.to(device)
+        
+        disc.zero_grad()
         noise = torch.randn((BATCH_SIZE,Z_DIM,1,1)).to(device)
         fake = gen(noise)
-        
         disc_real = disc(real).reshape(-1)
         loss_disc_real = criterion(disc_real,torch.ones_like(disc_real))
-        disc_fake = disc(fake).reshape(-1)
+        disc_fake = disc(fake.detach()).reshape(-1)
         loss_disc_fake = criterion(disc_fake,torch.zeros_like(disc_fake))
         loss_disc = (loss_disc_real+loss_disc_fake)/2
-        disc.zero_grad()
-        loss_disc.backward()
+        loss_disc.backward(retain_graph=True)
         opt_disc.step()
         
-        
-        output = disc(fake).reshape(-1)
-        loss_gen = criterion(output,torch.ones_like(output))
         gen.zero_grad()
+        noise2 = torch.randn((BATCH_SIZE,Z_DIM,1,1)).to(device)
+        fake2 = gen(noise2)
+        output = disc(fake2).reshape(-1)
+        loss_gen = criterion(output,torch.ones_like(output))
         loss_gen.backward()
         opt_gen.step()    
         
@@ -89,5 +90,5 @@ for epoch in range(NUM_EPOCHS):
                 writer_fake.add_image("fake",img_grid_fake, global_step=step)
             step += 1
 
-# Close the tensorboard writer
-writer.close()
+writer_real.close()
+writer_fake.close()
--- utils/utils_weights.py
+++ utils/utils_weights.py
@@ -2,5 +2,5 @@ from torch import nn
 
 def initialize_weights(model):
     for m in model.modules():
-        if isinstance(m, nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d):
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data,0.0,0.02)
